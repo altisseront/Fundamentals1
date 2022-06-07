c8fe7cbcc131075133d21372769755941786c010
altisseront <altisseront@gmail.com>
2022-06-07 02:01:55 -0400

    init

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+for(int i = 1; i <= 255; i++)
+{
+    Console.WriteLine(i);
+}
+
+for (int i = 1; i<=100; i++)
+{
+    Console.WriteLine(i);
+    if(i % 15 == 0)
+    {
+        Console.WriteLine("FizzBuzz");
+    }
+    else if(i % 5 == 0)
+    {
+        Console.WriteLine("Buzz");
+    }
+    else if(i % 3 == 0)
+    {
+        Console.WriteLine("Fizz");
+    }
+    else
+    {
+        
+    }
+}
